b9707b7d3c174a54ace562244cac2728349a6c9a
yordanos woldeyes <107896300+yordanosW@users.noreply.github.com>
2024-03-12 16:43:34 -0700

    feat: implement cookies-sesion

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { cloneDeep } = require('sequelize/types/lib/utils')
 const db = require("../models")
 const bcrypt = require('bcrypt')
 
@@ -13,8 +14,21 @@ router.post('/', async (req, res) => {
             message:`Could not find a user with the provided username and password `
         })
     }else{
+        req.session.userId = user.userId
         res.json({user})
     }
 })
-
+router.get('/profile',async (req,res)=>{
+    console.log(req.session.userId)
+ try {
+    let user = await User.findOne({
+        where:{
+            userId:req.session.userId
+        }
+    })
+    res.json(user)
+ } catch (error){
+    res.status(404).send(null)
+ }
+})
 module.exports = router
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,8 +6,10 @@ const { User } = db
 
 router.post('/', async (req, res) => {
     let {password, ...rest} =req.body;
-    const user = await User.create({...rest,
-    passwordDigest:await bcrypt.hash(password, 10)})
+    const user = await User.create({
+        ...rest,
+    passwordDigest:await bcrypt.hash(password, 10)
+})
     res.json(user)
 })
 
