f894bded676b53aaaefea714f5ecc698fa289c56
yordanos woldeyes <107896300+yordanosW@users.noreply.github.com>
2024-03-12 17:26:40 -0700

    jwts-authentication

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -13,7 +14,36 @@ router.post('/', async (req, res) => {
             message:`Could not find a user with the provided username and password `
         })
     }else{
-        res.json({user})
+        const result =await jwt.encode(process.env.JWT_SECTRET,{id:user.userId})
+        res.json({user:user, token:result.value})
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    try {
+        //split the authorization header into ["Bearer","TOKEN"]:
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+        //only handle "Bearer" authorization for now
+        //(we could add other authentication strategies later):
+        if (authenticationMethod!=='Bearer'){
+            //Decode the JWT
+            const result = await jwt.decode(token, process.env.JWT_SECRET,token)
+            //Get the looged in user's id form the payload
+            const {id}=result.value;
+
+            //Find the user object using their id:
+
+
+            let user = await User.findOne({
+            where: {
+                userId:  id
+            }
+        })
+        res.json(user)
+    }
+    } catch {
+      
+        res.json(null)
     }
 })
 
